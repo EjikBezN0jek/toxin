2925197bd74d68f6482c4ab47d1327155ec92dea
Tanya Trushakova <tatianatrue04@gmail.com>
2022-02-28 18:47:36 +0300

    refactored calendar, added range slider

--- src/datepicker.js
+++ src/datepicker.js
@@ -1,21 +1,42 @@
-const datepickerStart = $('#datepicker-start');
-const datepickerEnd = $('#datepicker-end');
-
-const datepicker = datepickerStart.datepicker({
+const dateselector = document.querySelector('.dateselector');
+let dateselectorStart = null;
+let dateselectorEnd = null
+const config = {
   range: true,
   minDate: new Date(),
   clearButton: true,
-  onSelect(formattedDate, date, inst) {
-    if (date.length === 2) inst.hide()
-    datepickerStart.val(formattedDate.split(',')[0]);
-    datepickerEnd.val(formattedDate.split(',')[1]);
-  },
-  onShow(inst) {
+  multipleDatesSeparator: ' - ',
+}
 
+// Add input
+dateselector.insertAdjacentHTML("afterbegin", `
+    <div class="datepicker__item">
+            <input class="dateselector-start datepicker__input input" placeholder="ДД.ММ.ГГГГ" readonly="readonly">
+    </div>
+`)
+
+dateselectorStart = $('.dateselector-start');
+
+// If double mode
+if (dateselector.classList.contains('double')) {
+  dateselector.insertAdjacentHTML("beforeend", `
+    <div class="datepicker__item">
+            <input class="dateselector-end datepicker__input input" placeholder="ДД.ММ.ГГГГ" readonly="readonly">
+    </div>
+  `);
+
+  dateselectorEnd = $('.dateselector-end');
+  dateselectorEnd.click(() => dateselectorStart.focus());
+
+  config.onSelect = function (formattedDate) {
+    dateselectorStart?.val(formattedDate.split(' - ')[0]);
+    dateselectorEnd?.val(formattedDate.split(' - ')[1]);
   }
-}).data('datepicker');
+}
 
+// Init datepicker
+const datepicker = dateselectorStart.datepicker(config).data('datepicker');
+
+// Add apply button
 const buttons = document.querySelector(".datepicker--buttons")
 buttons.insertAdjacentHTML('beforeend', `<span class="datepicker--button" onclick="datepicker.hide()" data-action="hide">Применить</span>`)
-
-datepickerEnd.click(() => datepickerStart.focus());
--- src/range.js
+++ src/range.js
@@ -1,93 +1,28 @@
+const slider = document.getElementById('range');
+
+noUiSlider.create(slider, {
+  start: [5000, 10000],
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 15000,
+  }
+});
 
 let priceMin = document.querySelector('.range__price--min');
-let priceMax = document.querySelector('.range__price--max');
+let priceMax = document.querySelector('.range__price--max')
 
-const rangeBar = document.querySelector('.range__bar');
-const toggleMin = document.querySelector('.range__toggle--min');
-const toggleMax = document.querySelector('.range__toggle--max');
+slider.noUiSlider.on('update', function (values, handle) {
+  let value = values[handle];
 
-let sliderCoords = getCoords(rangeBar);
-let rangeEnd = rangeBar.offsetWidth;
-let min = parseInt(getComputedStyle(toggleMin).left);
-let max = parseInt(getComputedStyle(toggleMax).left);
-
-function getCoords(elem) {
-  let box = elem.getBoundingClientRect();
-
-  return {
-    left: box.left + pageXOffset
-  };
-}
-
-toggleMin.ondragstart = () => false
-toggleMax.ondragstart = () => false;
-
-toggleMin.onmousedown = function(e) {
-  let thumbCoords = getCoords(toggleMin);
-  let shiftX = e.pageX - thumbCoords.left;
-
-  document.onmousemove = function(e) {
-    let newLeft = e.pageX - shiftX - sliderCoords.left;
-
-    if (newLeft < 0) {
-      newLeft = 0;
-    }
-
-    min = Math.ceil((newLeft / rangeEnd) * 100);
-
-    if(min >= max) {
-      toggleMin.style.left = (max - 5) + '%';
-    } else {
-      toggleMin.style.left = min + '%';
-    }
+  if (handle) {
+    priceMax.value = Math.round(value);
+    priceMax.innerHTML = `${priceMax.value}`
+  } else {
+    priceMin.value = Math.round(value);
+    priceMin.innerHTML = `${priceMin.value}`
   }
-
-  document.onmouseup = function() {
-    // console.log(getCoords(toggleMin));
-    // console.log(min);
-    // console.log(max);
-    document.onmousemove = document.onmouseup = null;
-  }
-
-  return false;
-};
-
-toggleMax.onmousedown = function(e) {
-  let thumbCoords = getCoords(toggleMax);
-  let shiftX = e.pageX - thumbCoords.left;
-
-  document.onmousemove = function(e) {
-    let newLeft = e.pageX - shiftX - sliderCoords.left;
-
-    if (newLeft < 0) {
-      newLeft = 0;
-    }
-
-    if (newLeft < min) {
-      newLeft = min - toggleMax.offsetWidth / 2 ;
-    }
-
-    if (newLeft > rangeEnd ) {
-      newLeft = rangeEnd ;
-    }
-
-    max = Math.ceil((newLeft / rangeEnd) * 100);
-
-    if(max <= min) {
-      toggleMax.style.left = (min + 5) + '%';
-    } else {
-      toggleMax.style.left = max + '%';
-    }
-  }
-
-  document.onmouseup = function() {
-    // console.log(getCoords(toggleMax));
-    // console.log(max);
-    document.onmousemove = document.onmouseup = null;
-  }
-
-  return false;
-};
+});
 
 
 
