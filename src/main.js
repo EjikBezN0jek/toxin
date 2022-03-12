d92d940b7bcc97f0cda65180f057598348bd0607
Tanya Trushakova <tatianatrue04@gmail.com>
2022-03-12 14:24:54 +0300

    refactored product page and header

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 const menuItem = document.querySelectorAll('.menu__item');
 
-function changePage(link) {
-  menuItem.forEach(link => link.classList.remove('menu__item--active'));
-  link.classList.add('menu__item--active');
+const burgerMenu = document.querySelector('.burger');
+const closeBtn = document.querySelector('.btn-close');
+const headerMenu = document.querySelector('.header__menu');
+const overlay =document.querySelector('.header__overlay');
+
+
+function toggleMenu() {
+  headerMenu.classList.toggle('header__menu--open');
+  burgerMenu.classList.toggle('burger');
+  burgerMenu.classList.toggle('btn-close');
+  overlay.classList.toggle('header__overlay--visible');
 }
 
-menuItem.forEach(link => link.addEventListener('click', (e) => changePage(e.target)))
+burgerMenu.addEventListener('click', toggleMenu)
+closeBtn.addEventListener('click', toggleMenu)
+
+// function changePage(link) {
+//   menuItem.forEach(link => link.classList.remove('menu__item--active'));
+//   link.classList.add('menu__item--active');
+// }
+//
+// menuItem.forEach(link => link.addEventListener('click', (e) => changePage(e.target)))
 
 
